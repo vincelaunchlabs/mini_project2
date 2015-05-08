7036460c4ca2c19eef02fdeab540de1a85b58861
Vince Banzon <vince@launchlabs.ph>
2015-05-08 19:16:10 +0800

    create-blog

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
+   get 'blogs_new' => 'blogs#new'
+   post 'blogs' => 'blogs#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
