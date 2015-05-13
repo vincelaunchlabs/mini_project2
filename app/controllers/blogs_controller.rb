dd23a6689a58cd330a4c79d11f023cbb3bee7e53
Vince Banzon <vince@launchlabs.ph>
2015-05-13 18:04:38 +0800

    public-blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def index
     @blog = Blog.where(user_id: current_user.id)
